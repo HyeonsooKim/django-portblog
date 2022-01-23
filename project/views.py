3180cd495e1037edd44c7e5cb985ccf8a39f11bc
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-23 16:57:23 +0900

    Add projects_index in views.py in project directory

--- project/views.py
+++ project/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
 # 내가 만든 사이트에 필요한 기능을 정의하는 곳
 # Create your views here.
+
+def project_index(request):
+    projects = Project.objects.all().order_by('-created_at')
+    context = {'projects': projects}
+    return render(request, 'project/project_index.html', context)
