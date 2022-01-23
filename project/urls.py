97189cada951a53b0cd0d1631c59e12a940f86d2
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-23 17:12:19 +0900

    Add content in base.html and project_index.html

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,17 @@
 from django.urls import path
 from . import views
 
+"""
+앱 자체의 주소 설정을 해줬으니 앱 안에서 메인화면과 디테일 화면의 세부 주소 형식을 설정해줘야함
+
+메인화면은 주소창에 /(슬래쉬)로 설정을 할 것이기 때문에 비워두고 name은 index로 설정해둠
+
+app_name은 다른 앱과의 차별화를 두기 위해 지정함
+
+project의 메인화면은 index 이름이고, blog 앱의 메인 화면도 index 이름을 가지면 헷갈리게 됨
+
+그래서 별도의 이름을 지정해줌
+"""
 app_name = 'project'
 urlpatterns =[
     path('', views.project_index, name='index')
