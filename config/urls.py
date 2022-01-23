40bf62ce1f7fbaf5279fb913a4d0329680818f69
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-23 16:58:40 +0900

    Add url of project in urls.py of config

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('project/', include('project.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
