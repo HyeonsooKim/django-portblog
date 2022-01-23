901ba8c527b50fccc19203a1e38b6100aa4fb761
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-23 16:49:26 +0900

    make superuser and add admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 # 장고에서 제공해주는 관리자 페이지 설정
 # Register your models here.
+
+# 내가 만든 DB를 가져와 어드민 사이트에서 등록을 하겠다
+admin.site.register(Project)
+
