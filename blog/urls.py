28e7716a67e4c2680c1ab02f9ab9caac696c5b48
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 01:03:12 +0900

    Add contents in urls.py in blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
+    path('', views.blog_index, name='index'),
+    path('detail/<int:id>/', views.blog_detail, name='detail'),
+    path('<category>/', views.blog_category, name='category'),
 ]
