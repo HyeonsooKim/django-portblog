faa42773dfcccdeb9486873c4dcb7ce1697795c0
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 02:00:57 +0900

    blog templates added

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 
 
 class Category(models.Model):
-    ame = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
