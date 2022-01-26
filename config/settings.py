f04eaf3dd553ba8020dbc649fab934e63839288f
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 05:00:51 +0900

    Update ALLOWED_HOSTS with site and development server URL

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ import os
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hspofol.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
