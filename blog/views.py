c751dc30c7357f27e1be92866264700e1806f092
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 00:51:56 +0900

    Add contents views.py in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_at")
+    context = {'posts': posts}
+    return render(request, 'blog/blog_index.html', context)
+
+def blog_detail(request, id):
+    post = Post.object.get(pk=id)
+    context = {'post': post}
+    return render(request, 'blog/blog_detail.html', context)
+
+def blog_category(request, category):
+    posts = Post.object.filter(categories__name__contains=category).order_by('-created_at')
+    context = {'posts': posts, 'category': category}
+    return render(request, 'blog/blog_category.html', context)
