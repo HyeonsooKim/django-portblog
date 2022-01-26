43887f439fd63c68bcb07ad6032d3ed6b8d7afbf
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-26 23:23:47 +0900

    Error fix extends html added 'project/'

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
