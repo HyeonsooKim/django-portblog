306a8bf5f80c62cbbc27fe8a63bf31b27bd36cff
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 00:36:18 +0900

    Add models.py in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('제목', max_length=255)
+    contents = models.TextField('내용')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
+
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    ame = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
