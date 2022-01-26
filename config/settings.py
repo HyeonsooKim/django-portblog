f76a14c87f3f5ab016e03049f3deac68ecd69665
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2022-01-27 05:06:25 +0900

    Change setting

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ import os
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['hspofol.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['hspofol.herokuapp.com/project', '127.0.0.1:8000/project']
 
 
 # Application definition
